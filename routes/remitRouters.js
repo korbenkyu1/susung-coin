065cae2134e04f71b3cc2cc548f0e6d2a5bf08b3
korbenkyu1 <108736444+korbenkyu1@users.noreply.github.com>
2023-10-20 16:17:01 +0900

    Update remitRouters.js

--- routes/remitRouters.js
+++ routes/remitRouters.js
@@ -27,21 +27,32 @@ router.post("/", async (req, res, next) => {
         pageTitle: "송금",
         userLoggedIn: req.session.user,
     }
+    const fromUser = await User.findOne({userID: from});
 
-    // 송금 대상이 없는지
-    if(await User.findOne({userID: to}) == null){
-        payload.pageTitle = "송금 실패";
-        payload.errorMessage = "송금 대상을 찾을 수 없습니다.";
-        return res.status(200).render("remit", payload);
+    // 관리자가 아니라면
+    if(fromUser.isAdmin === false){
+        toUser = await User.findOne({userID: to});
+        // 송금 대상이 없는지
+        if(toUser == null){
+            payload.pageTitle = "송금 실패";
+            payload.errorMessage = "송금 대상을 찾을 수 없습니다.";
+            return res.status(200).render("remit", payload);
+        }
+        // 학생, 부스간 
+        if(fromUser.isBooth === toUser.isBooth){
+            payload.pageTitle = "송금 실패";
+            payload.errorMessage = "잘못된 송금 대상입니다.";
+            return res.status(200).render("remit", payload);
+        }
+        
+        // 송금 금액이 충분한지
+        if(moneySent > 0 && req.session.user.money < moneySent){
+            payload.pageTitle = "송금 실패";
+            payload.errorMessage = "잔액이 부족합니다.";
+            return res.status(200).render("remit", payload);
+        }
     }
-    
-    // 송금 금액이 충분한지
-    if(req.session.user.money < moneySent){
-        payload.pageTitle = "송금 실패";
-        payload.errorMessage = "잔액이 부족합니다.";
-        return res.status(200).render("remit", payload);
-    }
-
+        
     await User.updateOne({userID: to}, {'$inc': {money: +moneySent}});
     req.session.user = await User.findOneAndUpdate({userID: from}, {'$inc': {money: -moneySent}}, {returnOriginal: false});
     console.log(req.session.user)
