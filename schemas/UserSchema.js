91b8d0616164d5da4a58ffc46cc8076d96c4f7b5
korbenkyu1 <108736444+korbenkyu1@users.noreply.github.com>
2023-10-24 15:25:25 +0900

    Update UserSchema.js

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema({
     userID:{ type: String, required: true, trim: true },
     password:{ type: String },
     money: { type:Number, default: 0},
-    isBooth: {type:Boolean, default: true},
+    isBooth: {type:Boolean, default: false},
     isAdmin: {type:Boolean, default: false}
 });
 var User = mongoose.model('User', UserSchema);
