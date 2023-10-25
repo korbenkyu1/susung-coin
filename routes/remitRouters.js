ea9a302a42e435b92b087cad1a8b83eba39d75c1
korbenkyu1 <108736444+korbenkyu1@users.noreply.github.com>
2023-10-26 00:36:29 +0900

    Update remitRouters.js

--- routes/remitRouters.js
+++ routes/remitRouters.js
@@ -42,8 +42,8 @@ router.post("/", async (req, res, next) => {
             payload.errorMessage = "올바르지 않은 송금 대상입니다.";
             return res.status(200).render("remit", payload);
         }
-        // 송금 금액이 양수인지
-        if(moneySent <= 0){
+        // 송금 금액이 올바른지
+        if(moneySent <= 0 || moneySent > 60000){
             payload.pageTitle = "송금 실패";
             payload.errorMessage = "올바르지 않은 송금 금액입니다.";
             return res.status(200).render("remit", payload);
@@ -67,7 +67,38 @@ router.post("/", async (req, res, next) => {
             return res.status(200).render("remit", payload);
         }
     }
-        
+    // 관리자 예외
+    else if(
+        [
+            "학생회팝콘",
+            "학생회소시지",
+            "학생회아이스티",
+            "학생회티켓응모",
+            "학생회상품응모",
+            "학생회상품응모두번째",
+            "학생회상품응모세번째"
+        ].includes(fromUser.userID)
+    ){
+        // 가져올 금액이 충분한지
+        if(toUser.money + moneySent < 0){
+            payload.pageTitle = "송금 실패";
+            payload.errorMessage = "잔액이 부족합니다.";
+            return res.status(200).render("remit", payload);
+        }
+
+        // 중복 구입 방지
+        const log = await Log.findOne({
+            $and: [
+                {from: fromUser.userID},
+                {to: toUser.userID}
+            ]
+        })
+        if(!toUser.isAdmin && log){
+            payload.pageTitle = "송금 실패";
+            payload.errorMessage = "이미 송금한 송금 대상입니다.";
+            return res.status(200).render("remit", payload);
+        }
+    }
     await User.updateOne({userID: toUser.userID}, {'$inc': {money: +moneySent}});
     req.session.user = await User.findOneAndUpdate({userID: fromUser.userID}, {'$inc': {money: -moneySent}}, {returnOriginal: false});
     var data = {
